2f4416c39272593769cd22f0b331f5799a6aad5b
Euhid Aman <aman.derax20@gmail.com>
2021-05-02 00:28:09 +0530

    Created & added styles, to each message to be sent

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import './App.css';
 import Message from "./components/Message";
 
-// 1:02:00
+// 1:19:00
 
 // REACT hooks -->
 // useState = it's like a vraible in REACT, which stores temporary memory
@@ -31,24 +31,26 @@ function App() {
 
   return (
     <div className="App container">
-      <h1>New to React !!</h1>
+      <h1>Messenger Clone</h1>
       <h1>Welcome {username}</h1>
 
       {/* We are wrapping the input & button in a form, bcz 'enter key' doesn't work without it */}
       <FormControl>
         <InputLabel>Message</InputLabel>
         <Input value={input} onChange={event => setInput(event.target.value)} />
-        <Button disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage} >Send</Button>
+        <Button disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage} >Send</Button><br />
       </FormControl>
       {/* The above code is used to continuously update and set the value of input to whtever is typed in the input field*/}
 
       {
         messages.map(message => (
-          <Message username={message.username} text={message.text}/>
+          <Message username={username} message={message}/>
         ))
       }
       {/* Above code loops through all the msgs in the messages array, and displays it.
-       username & text are props, which we are passing to the Message.js component */}
+       username & message are props, which we are passing to the Message.js component.
+       The username, is the person who's logged in. And, we are passing the whole message as a prop
+       because, a message can have a user which isn't the person logged in */}
 
     </div>
   );
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,15 +1,22 @@
 import { Card, CardContent, Typography } from '@material-ui/core'
 import React from 'react'
+import './Message.css';
 
 function Message(props) {
-    return (        
-        <Card>
-            <CardContent>
-                <Typography color='white' variant='h5' component='h2'>
-                    {props.username} : {props.text}
-                </Typography>
-            </CardContent>
-        </Card>
+
+    const isUser = props.username === props.message.username ;
+
+    return (    
+        <div className={`message_card ${isUser && 'message_user'}`}>
+            <Card className={isUser ? "message_userCard" : "message_guestCard"} variant="outlined">
+                {/* The code in className means, if the isUser variable is true, then the 'message_user' class is added */}
+                <CardContent>
+                    <Typography color='white' variant='h5' component='h2'>
+                        {props.message.username} : {props.message.text}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </div>          
     )
 }
 
