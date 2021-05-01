4510e304871545f8acda0e02a76be875477fde86
Euhid Aman <aman.derax20@gmail.com>
2021-05-01 12:47:32 +0530

    Created a basic frontend for inputting msgs and displaying thm.
    Created a state, for input and for messages.
    Created a form for taking in inputs and a button for submitting it.
    Also, used 'map' function to show every message separately.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,39 @@ import { useState } from "react";
 // import logo from './logo.svg';
 import './App.css';
 
+// 28:00
+
 function App() {
 
   const [input, setInput] = useState('');
+  console.log(input);
+  const [messages, setMessages] = useState([])
+
+  const sendMessage = (event) => { //Contains logic to send messages
+      setMessages([...messages, input]);//Saves all messages as an array
+      setInput(''); //After pressing send, Make textbox empty
+  }
+  console.log(messages);
 
   return (
     <div className="App">
       <h1>New to React !!</h1>
-      <input/>
-      <button>Send</button>
+
+      {/* We are wrapping the input & button in a form, bcz 'enter key' doesn't work without it */}
+      <form>
+          <input value={input} onChange={event => setInput(event.target.value)} />
+          <button type='submit' onClick={sendMessage} >Send</button>
+      </form>      
+      {/* The above code is used to continuously update and set the value of input to whtever is typed in the input field*/}
+
+      
+      
+      {
+        messages.map(message => (
+          <p>{message}</p>
+        ))
+      }
+
     </div>
   );
 }
