263d4e290e820dd30d42eff05bc7f1406d164364
Euhid Aman <aman.derax20@gmail.com>
2021-05-01 09:21:21 +0530

    Removed unnecessary initial files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { useState } from "react";
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [input, setInput] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>New to React !!</h1>
+      <input/>
+      <button>Send</button>
     </div>
   );
 }
